4d6a6dae9370f8bd22590d75ca2481a8c5452ce2
Rusooo <developers93@gmail.com>
2019-12-16 02:50:06 +0400

    master

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,8 @@
 $(document).ready(function () {
+
     $(".datepicker").datepicker({
         'format': 'dd-mm-yyyy',
         'autoclose': true
     });
 
-    $(".menu ul li a").hover(function () {
-
-
-        var str = $(this).find("li").index();
-
-        $(this).eq(str).css("color", "#c8c8c8");
-
-
-    }).mouseleave(function () {
-        this.css("color", "#fff");
-    });
 });
